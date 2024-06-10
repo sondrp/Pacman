457fac6eb68eda5bae9d959ac20ce6d763d6dea6
Sondre Pedersen <sondrp@stud.ntnu.no>
2024-06-10 11:09:52 +0200

    simple frontend showing game state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,45 @@
 import { useState } from "react";
 import { cn } from "./utils/cn";
+import { useWebSocket } from "./hooks/useWebSocket";
 
-const BOARD = `bbbbbbbbbbbbbbbbbbb/bfffbffff  fffbfffb/bfbfbfbbbbbbbbbfbfb/bffffp fffffffffffb/bbbbbfbbfbfbbbbbbfb/bbbbbbbbffffffffffb/bbbbbbbbbbbbbbbbbbb`;
-
-type Action = {
-  when: RegExp,
-  replacement: string,
-}
-
-
-const actions: Record<string ,Action> = {
-  'ArrowRight': {when: /p[f ]/, replacement: ' p'},
-  'ArrowLeft': {when: /[f ]p/, replacement: 'p '},
-  'ArrowDown': {when: /p(.{19})[f ]/, replacement: ' $1p'},
-  'ArrowUp': {when: /[f ](.{19})p/, replacement: 'p$1 '}
-} as const
+const actions: Record<string, string> = {
+  ArrowRight: "E",
+  ArrowLeft: "W",
+  ArrowDown: "S",
+  ArrowUp: "N",
+} as const;
 
 function App() {
-  const [board, setBoard] = useState(BOARD)
+  const { ws, message: board, refresh } = useWebSocket();
 
   const handleKeyDown = (e: React.KeyboardEvent) => {
-    if (!(e.key in actions)) return
+    if (!(e.key in actions)) return;
+    if (!ws) return;
 
-    const { when, replacement } = actions[e.key]
-    if (!when.test(board)) return
-
-    setBoard(board.replace(when, replacement))
-  }
+    const action = actions[e.key];
+    ws.send(action);
+  };
 
   return (
-    <button onKeyDown={handleKeyDown} className="flex cursor-default h-screen w-full items-center justify-center bg-blue-300">
-      <div className="flex w-[47.5rem] flex-wrap">
-        {board.split("").filter(l => l !== "/").map((value, i) => (
-          <Square key={i} value={value} />
+    <button
+      onKeyDown={handleKeyDown}
+      className="relative flex h-screen w-full cursor-default items-center justify-center bg-blue-300"
+    >
+      {!ws && (
+        <button
+          onClick={refresh}
+          className="absolute left-20 top-20 rounded-md border border-black px-4 py-2"
+        >
+          Lost connection. Retry?
+        </button>
+      )}
+      <div className="">
+        {board.split("/").map((row, rowId) => (
+          <div key={rowId} className="flex">
+            {row.split("").map((value, colId) => (
+              <Square key={rowId + " " + colId} value={value} />
+            ))}
+          </div>
         ))}
       </div>
     </button>
@@ -44,19 +51,33 @@ function Square({ value }: { value: string }) {
     <div
       className={cn(
         "flex size-10 items-center justify-center bg-blue-800",
-        value === "b" && "bg-black",
+        value === "x" && "bg-black",
       )}
     >
       {value === "f" && <div className="size-2 rounded-full bg-white"></div>}
-      {value === "p" && <Pacman />}
+      {value === "P" && <Pacman />}
+      {value === "b" && <Ghost color="bg-red-500" />}
+      {value === "p" && <Ghost color="bg-pink-500" />}
+      {value === "i" && <Ghost color="bg-cyan-500" />}
+      {value === "c" && <Ghost color="bg-orange-500" />}
     </div>
   );
 }
 
+function Ghost({ color }: { color: string }) {
+  return (
+    <div
+      className={cn("relative size-8 overflow-hidden rounded-full", color)}
+    ></div>
+  );
+}
+
 function Pacman() {
-  return <div className="size-8 overflow-hidden rounded-full bg-yellow-400 relative">
-    <div className="absolute size-4 bg-blue-800 rotate-45 top-2 -right-2"></div>
-  </div>
+  return (
+    <div className="relative size-8 overflow-hidden rounded-full bg-yellow-400">
+      <div className="absolute -right-2 top-2 size-4 rotate-45 bg-blue-800"></div>
+    </div>
+  );
 }
 
 export default App;
