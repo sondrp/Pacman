4eb9b0d7c57c9784598c72ee41ed3f71e86215f7
Sondre Pedersen <sondrp@stud.ntnu.no>
2024-06-15 11:07:32 +0200

    update board page to use websockets and live board

--- frontend/src/pages/BoardPage.tsx
+++ frontend/src/pages/BoardPage.tsx
@@ -1,16 +1,42 @@
 import { useParams } from "react-router-dom";
 import BoardView from "../components/BoardView";
+import { useWebSocket } from "../hooks/useWebSocket";
+import { useEffect } from "react";
 
-// TODO : fix the params. I would like to not use / in the board definition, as it messes with the path
+const keyboardMap: Record<string, string> = {
+  ArrowRight: "E",
+  ArrowDown: "S",
+  ArrowLeft: "W",
+  ArrowUp: "N",
+};
 
 export default function BoardPage() {
   const { id } = useParams();
   if (!id) throw Error("bad params");
 
-  const board = id.replace(/-/g, "/");
+  const { ws, board, refresh } = useWebSocket(id);
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (!ws) return;
+      if (!(e.key in keyboardMap)) return;
+      ws.send(keyboardMap[e.key]);
+    };
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [ws]);
 
   return (
-    <div className="bg-blue flex grow items-center justify-center bg-blue-300">
+    <div className="bg-blue relative flex grow items-center justify-center bg-blue-300">
+      {!ws && (
+        <button
+          onClick={refresh}
+          className="absolute left-10 top-10 rounded-md border px-4 py-2"
+        >
+          Lost connection. Retry?
+        </button>
+      )}
       <BoardView board={board} />
     </div>
   );
